5a86edd3bce2b2041dc1e365a3cd699fc06955a4
NitrusAphalion <cryptobeardsman@gmail.com>
2024-09-17 15:22:38 -0600

    fix

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,28 +1,31 @@
-'use client'
+'use client';
 
 /**
+
  * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
+
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import { visionTool } from '@sanity/vision';
+import { structureTool } from 'sanity/structure';
+import { defineConfig } from 'sanity';
+import { markdownSchema } from 'sanity-plugin-markdown';
+import { singletonTools } from 'sanity-plugin-singleton-tools';
+import { apiVersion, dataset, projectId } from './src/sanity/env';
+import { schema } from './src/sanity/schema';
+import { media } from 'sanity-plugin-media';
 
 export default defineConfig({
+  name: 'default',
   basePath: '/studio',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
   plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    structureTool(),
+    visionTool({ defaultApiVersion: apiVersion }),
+    markdownSchema(),
+    singletonTools(),
+    media(),
   ],
-})
+  schema,
+});
